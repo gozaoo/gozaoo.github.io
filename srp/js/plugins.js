a65f53d579a510b54979e1b5bf61af63f7251925
Doge <81862662+gozaoo@users.noreply.github.com>
2022-02-26 13:58:23 +0800

    Delete srp directory

--- srp/js/plugins.js
+++
@@ -1,5 +0,0 @@
-function FunAlert(Title, Text ,id) {
-    var doc = document.getElementById('alertOutSide')
-    doc.innerHTML = '<div class="alertBox" id="'+id+'"><h1>' + Title + '</h1><div class = "alertBoxText" ><div class = "alertBoxTextBox" >'+Text + '</div></div></div>'
-    console.log(doc);
-}
